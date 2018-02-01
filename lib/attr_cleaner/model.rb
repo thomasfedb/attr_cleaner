0e0bfb788254a4ed23fcb79e504f02b96b9828d5
Thomas Drake-Brockman <thom@sfedb.com>
2018-02-01 13:02:29 +0800

    Change implementation to avoid alias_method_chain

--- lib/attr_cleaner/model.rb
+++ lib/attr_cleaner/model.rb
@@ -1,24 +1,23 @@
 module AttrCleaner
   module Model
     extend ActiveSupport::Concern
-
-    included do
-      alias_method_chain :write_attribute, :cleaner
-      class_attribute :attr_cleaners
-    end
-
-    def write_attribute_with_cleaner(attr_name, value)
-      if Array(attr_cleaners).include?(attr_name.to_sym) && value.is_a?(String)
-        value = value.strip
-        value = nil if value.empty?
-      end
-
-      write_attribute_without_cleaner(attr_name, value)
-    end
-
+    
     module ClassMethods
       def attr_cleaner(*columns)
-        self.attr_cleaners = columns
+        columns.each do |column|
+          define_attr_cleaner(column)
+        end
+      end
+
+      def define_attr_cleaner(column)
+        define_method "#{column}=" do |value|
+          if value.is_a?(String)
+            value = value.strip
+            value = nil if value.empty?
+          end
+
+          super(value)
+        end
       end
     end
   end
